95d425d403e92f51d5db3cb228136fdea10976f6
Abdur Rehman <162489957+Abdur-Rehman15@users.noreply.github.com>
2025-08-05 17:01:53 +0500

    Create 3477. Fruits Into Baskets II.cpp

---
+++ 3477. Fruits Into Baskets II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+
+        int n = fruits.size(), count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (baskets[j] && baskets[j] >= fruits[i]) {
+                    count++;
+                    baskets[j] = 0;
+                    break;
+                }
+            }
+        }
+        return n - count;
+    }
+};
