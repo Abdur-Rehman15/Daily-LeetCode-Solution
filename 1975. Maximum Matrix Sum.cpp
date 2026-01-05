5cc52b497ccec47949de7240ed2400f69e7c42dc
Abdur Rehman <162489957+Abdur-Rehman15@users.noreply.github.com>
2026-01-06 03:07:03 +0500

    Create 1975. Maximum Matrix Sum.cpp

---
+++ 1975. Maximum Matrix Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+
+        long long sum = 0;
+        int mini = INT_MAX, n = matrix.size(), negCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += abs(matrix[i][j]);
+                mini = min(mini, abs(matrix[i][j]));
+                if (matrix[i][j] < 0) negCount++;
+            }
+        }
+
+        return (negCount % 2 == 0) ? sum : sum - (mini * 2);
+    }
+};
